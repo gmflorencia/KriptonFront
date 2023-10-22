f5f8ab84f24e7b92e0c651b35b934b84cc978781
gmflorencia <gonzalezmartinezflorencia@gmail.com>
2023-10-22 18:44:09 -0300

    feat: Creación de Models y Controllers

---
+++ KriptonFront/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Data.DTOs;
+using Data.Base;
+using Newtonsoft.Json;
+
+
+
+namespace KriptonFront.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IHttpClientFactory _httpClient;
+        public LoginController(IHttpClientFactory httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Ingresar(LoginDto login)
+        {
+            var baseApi = new BaseApi(_httpClient);
+            var token = await baseApi.PostToApi("Login", login);
+            var resultadoLogin = token as OkObjectResult;
+            var resultadoObjeto = JsonConvert.DeserializeObject<Models.Login>(resultadoLogin.Value.ToString());
+            ViewBag.Nombre = resultadoObjeto.Nombre;
+            return View("~/Views/Home/Index.cshtml", resultadoObjeto);
+        }
+    }
+}
---
+++ KriptonFront/Models/Login.cs
@@ -0,0 +1,9 @@
+﻿namespace KriptonFront.Models
+{
+    public class Login
+    {
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+    }
+}
