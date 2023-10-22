899fc5e67e1a6648336edc131eb9c4d2d32873d6
gmflorencia <gonzalezmartinezflorencia@gmail.com>
2023-10-22 18:43:02 -0300

    feat: Creación de csproj Data

---
+++ Data/DTOs/LoginDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.DTOs
+{
+    public class LoginDto
+    {
+
+        public string Email { get; set; }
+        public string Clave { get; set; }
+    }
+}
